83b635ca2d0c7d6c46064e37acc78c747385e1c7
webclaw <bonwiltonscott@yahoo.com>
2014-10-16 15:22:43 +1300

    StartMenu script added

---
+++ Terraformer/assets/StartMenu/StartMenuOpenLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenuOpenLevel : MonoBehaviour {
+
+
+	
+
+	void Update () {
+		if (Input.anyKey) {
+						Application.LoadLevel ("TerraformerMap");
+				}
+	}
+}
